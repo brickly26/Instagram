ce78b88016f3cfecbfa428afcd9a971e29f3f26a
Burak Aksu <sabburak26@gmail.com>
2021-11-18 21:34:40 -0500

    added skeleton to profile pic and username on sidebar

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,3 +1,31 @@
-export default function User() {
-  return <p>I am a user</p>;
-}
+import { memo } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
+
+const User = ({ username, fullName }) =>
+  !username || !fullName ? (
+    <Skeleton count={1} height={61} />
+  ) : (
+    <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4">
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src={`/images/avatars/${username}.jpeg`}
+          alt=""
+        />
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
+      </div>
+      <p>{username}</p>
+    </Link>
+  );
+
+export default User;
+
+User.propTypes = {
+  username: PropTypes.string.isRequired,
+  fullName: PropTypes.string.isRequired
+};
