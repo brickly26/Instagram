95b0af8281ec4b5a15451f59da3c8c1a14a02c50
Burak Aksu <sabburak26@gmail.com>
2021-11-19 08:25:27 -0500

    profile page setup

---
+++ src/hooks/use-photos.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+import { getPhotos } from '../services/firebase';
+
+export default function usePhotos(user) {
+  const [photos, setPhotos] = useState(null);
+
+  useEffect(() => {
+    async function getTimeLinePhotos() {
+      if (user?.following?.length > 0) {
+        const followedUserPhotos = await getPhotos(user.userId, user.following);
+
+        followedUserPhotos.sort((a, b) => b.dateCreated - a.dataCreated);
+        setPhotos(followedUserPhotos);
+      }
+    }
+
+    getTimeLinePhotos();
+  }, [user?.userId, user?.following]);
+
+  return { photos };
+}
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,19 +1,20 @@
 import { useEffect } from 'react';
-import Proptypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Header from '../components/header';
 import Timeline from '../components/timeline';
 import Sidebar from '../components/sidebar';
 import useUser from '../hooks/use-user';
 import LoggedInUserContext from '../context/logged-in-user';
 
-export default function dashboard() {
+export default function Dashboard({ user: loggedInUser }) {
   const { user, setActiveUser } = useUser(loggedInUser.uid);
   useEffect(() => {
     document.title = 'Instagram';
   }, []);
 
   return (
-      <LoggedInUserContext.Provider value={{ user, setActiveUser }}>
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <LoggedInUserContext.Provider value={{ user, setActiveUser }}>
       <div className="bg-gray-background">
         <Header />
         <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
@@ -25,6 +26,6 @@ export default function dashboard() {
   );
 }
 
-dashboard.propTypes = {
-  user:Proptypes.object.isRequired
+Dashboard.propTypes = {
+  user: PropTypes.object.isRequired
 };
